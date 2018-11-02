1bf4610739e189de93d488e0015db3e3f5612d62
Kyle_Tecson <kyltecs@gmail.com>
2018-11-02 10:46:51 -0700

    add ball bounce off walls

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -23,24 +23,43 @@ export default class Ball {
     this.vx = this.direction * (6 - Math.abs(this.vy));
   }
 
+  wallCollision() {
+    const hitLeft = this.x - this.radius <= 0;
+    const hitRight = this.x + this.radius >= this.boardWidth;
+    const hitTop = this.y - this.radius <= 0;
+    const hitBottom = this.y + this.radius >= this.boardHeight;
+
+    if (hitTop) {
+      this.vy *= -1;
+    } else if (hitBottom) {
+      this.vy *= -1;
+    } else if (hitLeft) {
+      this.vx *= -1;
+    } else if (hitRight) {
+      this.vx *= -1;
+    }
+  }
+
   render(svg) {
     this.x += this.vx;
     this.y += this.vy;
+
+    this.wallCollision();
 
     let ball = document.createElementNS(SVG_NS, "circle");
     ball.setAttributeNS(null, "fill", this.currentColor[this.counter]);
     ball.setAttributeNS(null, "r", this.radius);
     ball.setAttributeNS(null, "cx", this.x);
     ball.setAttributeNS(null, "cy", this.y);
-    // this.timer++;
-    // if (this.timer === 260) {
-    //   this.counter++;
-    //   if (this.counter > 1) {
-    //     this.counter = 0;
-    //   } else if (this.timer > 260) {
-    //     this.counter = 0;
-    //   }
-    //  }
+    this.timer++;
+    if (this.timer === 260) {
+      this.counter++;
+      if (this.counter > 1) {
+        this.counter = 0;
+      } else if (this.timer > 260) {
+        this.counter = 0;
+      }
+    }
 
     svg.appendChild(ball);
   }
